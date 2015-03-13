81d0556ea6d4e533e3f57717c75849334ea88e71
zhuyue1219 <zjiang@my.okcu.edu>
2015-03-12 19:05:48 -0500

    I added the 3rd line in hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,6 @@
 main()
 {	printf("This is my first use of git. \n");
-	printf("This is the second modification. \n");
+
+printf("This is the second modification. \n");
+printf("I pulled the hello.c and added the local file"\n);
 }
